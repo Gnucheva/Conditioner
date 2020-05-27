5395e3a82e4b947cea846404535eeb331577fbf0
Anastasia <n41111215@mail.ru>
2020-05-27 17:26:42 +0300

    Add java code from lecture

---
+++ src/test/java/ru/netology/ConditionerAdvancedTest.java
@@ -0,0 +1,18 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerAdvancedTest {
+
+    public void shouldGetAndSet() {
+        ConditionerAdvanced conditioner = new ConditionerAdvanced();
+        String expected = "Кондишн";
+
+        assertNull(conditioner.getName());
+        conditioner.setName(expected);
+        assertEquals(expected, conditioner.getName());
+    }
+
+}
